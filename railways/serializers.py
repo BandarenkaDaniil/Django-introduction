387f0dec9e870afcbb13c3a4e8b786fcbb7dce28
Daniil Bondarenko <dbondarenko.post@gmail.com>
2018-12-10 15:31:21 +0300

    minor codestyle changes

--- railways/serializers.py
+++ railways/serializers.py
@@ -15,15 +15,21 @@ class TrainSerializer(serializers.ModelSerializer):
 class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
-        fields = ('id', 'title', 'country', 'longitude',
-                  'latitude', 'created_at', 'updated_at')
-        read_only_fields = ('id', 'longitude', 'latitude',
-                            'created_at', 'updated_at',)
+        fields = (
+            'id', 'title', 'country', 'longitude',
+            'latitude', 'created_at', 'updated_at'
+        )
+        read_only_fields = (
+            'id', 'longitude', 'latitude',
+            'created_at', 'updated_at',
+        )
 
 
 class RideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
-        fields = ('id', 'user', 'train', 'departure_station',
-                  'arrival_station', 'created_at', 'updated_at')
+        fields = (
+            'id', 'user', 'train', 'departure_station',
+            'arrival_station', 'created_at', 'updated_at'
+        )
         read_only_fields = ('id', 'created_at', 'updated_at',)
