b34a46b64313bfa09c2cf28fd40dea56d968027c
Daniil Bondarenko <dbondarenko.post@gmail.com>
2018-12-10 15:31:21 +0300

    minor codestyle changes

--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,8 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name',
-                  'created_at', 'updated_at')
+        fields = (
+            'id', 'email', 'first_name', 'last_name',
+            'created_at', 'updated_at'
+        )
         read_only_fields = ('id', 'created_at', 'updated_at',)
