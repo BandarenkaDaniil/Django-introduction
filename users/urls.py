b3694542f3902e5ec72acd2389f4bd83ca620d8b
Daniil Bondarenko <dbondarenko.post@gmail.com>
2018-12-10 13:51:01 +0300

    minor codestyle changes

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,8 @@ from users import views
 
 
 urlpatterns = [
-    path('', views.UserList.as_view()),
-    path('<int:pk>/', views.UserDetail.as_view()),
+    path('', views.UserListAPI.as_view()),
+    path('<int:pk>/', views.UserDetailAPI.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
