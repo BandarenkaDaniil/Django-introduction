75faca25aa11889af9ece6629416b996f1bb18c2
Daniil Bondarenko <dbondarenko.post@gmail.com>
2018-12-11 13:45:38 +0300

    delete "first_name" and "last_name" fields overriding and return to the previous migrate

--- users/models.py
+++ users/models.py
@@ -46,8 +46,6 @@ class User(AbstractUser, TimestampableModelMixin):
 
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
