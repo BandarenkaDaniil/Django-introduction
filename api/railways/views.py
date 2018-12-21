1bdf83f53049fe7049ea6172890aa3cb4fdaff13
Daniil Bondarenko <dbondarenko.post@gmail.com>
2018-12-21 14:37:01 +0300

    modify database structure and adapt models, urlconf, serializers and views

--- api/railways/views.py
+++ api/railways/views.py
@@ -2,41 +2,46 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 
 from api.railways.serializers import (
-    TrainSerializer,
+    # TrainSerializer,
     StationSerializer,
-    RideSerializer
+    TrackSerializer,
+    RouteSerializer,
+    RouteItemSerializer
 )
 
-from railways.models import Train
+# from railways.models import Train
 from railways.models import Station
-from railways.models import Ride
+from railways.models import Track
+from railways.models import Route
+from railways.models import RouteItem
+
 
 from railways.permissions import IsSuperUserOrReadOnly
 
 
-class TrainListAPI(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated, )
-
-    queryset = Train.objects.all()
-    serializer_class = TrainSerializer
-
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-
-        queryset = self.get_queryset()
-        serializer = TrainSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-
-class TrainDetailAPI(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsSuperUserOrReadOnly, )
-
-    queryset = Train.objects.all()
-    serializer_class = TrainSerializer
+# class TrainListAPI(generics.ListCreateAPIView):
+#     permission_classes = (permissions.IsAuthenticated, )
+#
+#     queryset = Train.objects.all()
+#     serializer_class = TrainSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         super().get(request, *args, **kwargs)
+#
+#         queryset = self.get_queryset()
+#         serializer = TrainSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#
+# class TrainDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsSuperUserOrReadOnly, )
+#
+#     queryset = Train.objects.all()
+#     serializer_class = TrainSerializer
 
 
 class StationListAPI(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.IsAuthenticated,)
 
     queryset = Station.objects.all()
     serializer_class = StationSerializer
@@ -50,28 +55,70 @@ class StationListAPI(generics.ListCreateAPIView):
 
 
 class StationDetailAPI(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsSuperUserOrReadOnly, )
+    # permission_classes = (IsSuperUserOrReadOnly, )
 
     queryset = Station.objects.all()
     serializer_class = StationSerializer
 
 
-class RideListAPI(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class TrackListAPI(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
 
-    queryset = Ride.objects.all()
-    serializer_class = RideSerializer
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
 
     def get(self, request, *args, **kwargs):
         super().get(request, *args, **kwargs)
 
         queryset = self.get_queryset()
-        serializer = RideSerializer(queryset, many=True)
+        serializer = TrackSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
-class RideDetailAPI(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsSuperUserOrReadOnly,)
+class TrackDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsSuperUserOrReadOnly, )
 
-    queryset = Ride.objects.all()
-    serializer_class = RideSerializer
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
+
+
+class RouteListAPI(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        queryset = self.get_queryset()
+        serializer = RouteSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class RouteDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsSuperUserOrReadOnly,)
+
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class RouteItemListAPI(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    queryset = RouteItem.objects.all()
+    serializer_class = RouteItemSerializer
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        queryset = self.get_queryset()
+        serializer = RouteItemSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class RouteItemDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsSuperUserOrReadOnly,)
+
+    queryset = RouteItem.objects.all()
+    serializer_class = RouteItemSerializer
--- railways/admin.py
+++ railways/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Train
-from .models import Ride
+# from .models import Train
 from .models import Station
+from .models import Track
+from .models import Route
+from .models import RouteItem
 
-admin.site.register(Train)
-admin.site.register(Ride)
+# admin.site.register(Train)
 admin.site.register(Station)
+admin.site.register(Track)
+admin.site.register(Route)
+admin.site.register(RouteItem)
 
