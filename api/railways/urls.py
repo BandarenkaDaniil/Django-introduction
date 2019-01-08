64fea74774ca28f6fb46572b5cfa84d4d8f9f386
Daniil Bondarenko <dbondarenko.post@gmail.com>
2019-01-08 13:43:14 +0300

    modify api structure (urlconf, views and serializers) according to new models

--- api/railways/urls.py
+++ api/railways/urls.py
@@ -8,17 +8,24 @@ urlpatterns = [
     # path('route_items/', views.RouteItemListAPI.as_view()),
     # path('route_items/<int:pk>/', views.RouteItemDetailAPI.as_view()),
 
+    path('rides/', views.RideListAPI.as_view()),
+    path('rides/<int:pk>/', views.RideDetailAPI.as_view()),
+
     path('routes/', views.RouteListAPI.as_view()),
     path('routes/<int:pk>/', views.RouteDetailAPI.as_view()),
 
     path('stations/', views.StationListAPI.as_view()),
     path('stations/<int:pk>/', views.StationDetailAPI.as_view()),
 
-    # path('trains/', views.TrainListAPI.as_view()),
-    # path('trains/<int:pk>/', views.TrainDetailAPI.as_view()),
+    path('tickets/', views.TicketListAPI.as_view()),
+    path('tickets/<int:pk>/', views.TicketDetailAPI.as_view()),
+
+    path('trains/', views.TrainListAPI.as_view()),
+    path('trains/<int:pk>/', views.TrainDetailAPI.as_view()),
 
     path('tracks/', views.TrackListAPI.as_view()),
     path('tracks/<int:pk>/', views.TrackDetailAPI.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/railways/views.py
+++ api/railways/views.py
@@ -2,32 +2,36 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 
 from api.railways.serializers import (
+    RideSerializer,
     RouteSerializer,
     RouteItemSerializer,
     StationSerializer,
+    TicketSerializer,
+    TrainSerializer,
     TrackSerializer,
-    # TrainSerializer,
 )
 
+from railways.models import Ride
 from railways.models import Route
 from railways.models import RouteItem
 from railways.models import Station
+from railways.models import Ticket
+from railways.models import Train
 from railways.models import Track
-# from railways.models import Train
 
 
-# class TrainListAPI(generics.ListCreateAPIView):
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-#
-#     queryset = Train.objects.all()
-#     serializer_class = TrainSerializer
-#
-#
-# class TrainDetailAPI(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-#
-#     queryset = Train.objects.all()
-#     serializer_class = TrainSerializer
+class TrainListAPI(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class TrainDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
 
 
 class StationListAPI(generics.ListCreateAPIView):
@@ -84,3 +88,31 @@ class RouteItemDetailAPI(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = RouteItem.objects.all()
     serializer_class = RouteItemSerializer
+
+
+class RideListAPI(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAdminUser,)
+
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer
+
+
+class RideDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAdminUser,)
+
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer
+
+
+class TicketListAPI(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAdminUser,)
+
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class TicketDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAdminUser,)
+
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
