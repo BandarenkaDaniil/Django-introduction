20163f15cd36db3b77fd525591847e12cd24675f
Daniil Bondarenko <dbondarenko.post@gmail.com>
2019-01-08 12:21:32 +0300

    add Train model

--- railways/admin.py
+++ railways/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-# from .models import Train
+from .models import Train
 from .models import Station
 from .models import Track
 from .models import Route
 from .models import RouteItem
 
-# admin.site.register(Train)
+admin.site.register(Train)
 admin.site.register(Station)
 admin.site.register(Track)
 admin.site.register(Route)
