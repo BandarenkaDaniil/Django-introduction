a2ce2e00d7b8be64b7ac9330f2bc9146baa424cd
Daniil Bondarenko <dbondarenko.post@gmail.com>
2019-01-08 12:45:50 +0300

    add Ticket model

--- railways/admin.py
+++ railways/admin.py
@@ -4,6 +4,7 @@ from .models import Ride
 from .models import Route
 from .models import RouteItem
 from .models import Station
+from .models import Ticket
 from .models import Train
 from .models import Track
 
@@ -12,6 +13,7 @@ admin.site.register(Ride)
 admin.site.register(Route)
 admin.site.register(RouteItem)
 admin.site.register(Station)
+admin.site.register(Ticket)
 admin.site.register(Train)
 admin.site.register(Track)
 
