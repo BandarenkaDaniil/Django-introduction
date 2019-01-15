b30e18820ee58faa6b16bd8a8f979104b53080d7
Daniil Bondarenko <dbondarenko.post@gmail.com>
2019-01-15 13:50:16 +0300

    fix authentication bug

--- django_introduction/settings.py
+++ django_introduction/settings.py
@@ -123,6 +123,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
