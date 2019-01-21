a8fdc32bb7a9ce749778cc4e4f4d5e429d003893
Daniil Bondarenko <dbondarenko.post@gmail.com>
2019-01-21 13:31:47 +0300

    alter urlconf

--- django_introduction/urls.py
+++ django_introduction/urls.py
@@ -7,6 +7,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('railways/', include('railways.urls')),
+    path('', include('railways.urls')),
     path('users/', include('users.urls')),
 ]
--- railways/urls.py
+++ railways/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from rest_framework.urlpatterns import format_suffix_patterns
+from .views import HomePage
 
 
 urlpatterns = [
+    path('', HomePage.as_view())
 ]
